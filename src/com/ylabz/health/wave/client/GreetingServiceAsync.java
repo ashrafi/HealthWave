73b2589eef75e1b2594cd65a4edef5fdbe8b7b7b
ashrafi <ashrafi@192.168.1.6>
2009-07-31 23:59:28 -0700

    initial commit

---
+++ src/com/ylabz/health/wave/client/GreetingServiceAsync.java
@@ -0,0 +1,10 @@
+package com.ylabz.health.wave.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback);
+}
